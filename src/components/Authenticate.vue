00e51ab69235480b30e1d693a2db31ca8bb78552
Keti Sula <ketisulamanidze@gmail.com>
2023-11-19 19:12:55 +0100

    adding login page

---
+++ src/components/Authenticate.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <form @submit="submitForm">
+            <label for="username">Username:</label>
+            <input type="text" id="username" v-model="username" required>
+            <br>
+            <label for="password">Password:</label>
+            <input type="password" id="password" v-model="password" required>
+            <br>
+            <button type="submit">Sign In</button>
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            username: '',
+            password: ''
+        };
+    },
+    methods: {
+        submitForm(event) {
+            event.preventDefault();
+            // Perform authentication logic here
+            console.log('Username:', this.username);
+            console.log('Password:', this.password);
+            // Reset form fields
+            this.username = '';
+            this.password = '';
+        }
+    }
+};
+</script>
+
+<style scoped>
+h1 {
+    text-align: center;
+}
+form {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 20px;
+}
+label {
+    margin-bottom: 10px;
+}
+input {
+    padding: 5px;
+    margin-bottom: 10px;
+}
+button {
+    padding: 10px 20px;
+    background-color: #007bff;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -3,10 +3,16 @@ import VueRouter from "vue-router";
 import Skull from "../components/Skull.vue";
 import AppAccounts from "../components/AppAccounts.vue";
 import HomePage from "../components/HomePage.vue";
+import Authentication from "../components/Authenticate.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
+  {
+    path: "/",
+    name: "Authenticate",
+    component: Authentication,
+  },
   {
     path: "/skull",
     name: "Skull",
@@ -18,7 +24,7 @@ const routes = [
     component: AppAccounts,
   },
   {
-    path: "/",
+    path: "/HomePage",
     name: "HomePage",
     component: HomePage,
   },
