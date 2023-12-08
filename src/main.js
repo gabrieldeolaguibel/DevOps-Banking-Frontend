340429b65b65a1a6f330f21cd0befb04d786304c
felixggj <me@felix-gomez.com>
2023-12-08 19:40:45 +0100

    Added logs for application insights

--- src/main.js
+++ src/main.js
@@ -1,13 +1,23 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
-//install bootstrap first
 import "bootstrap/dist/css/bootstrap.css";
+// install bootstrap first
 import BootstrapVue from "bootstrap-vue";
 
-Vue.config.productionTip = false;
+// Logs to Frontend
+import { ApplicationInsights } from "@microsoft/applicationinsights-web";
 
+Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
+
+const appInsights = new ApplicationInsights({
+  config: {
+    instrumentationKey: process.env.VUE_APP_APPINSIGHTS_INSTRUMENTATIONKEY,
+  },
+});
+appInsights.loadAppInsights();
+appInsights.trackPageView(); // Tracks page views
 
 new Vue({
   router,
